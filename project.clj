13e428d338c068c1ad4629553bed0a32b74c0b06
Ravindra R. Jaju <jaju@msync.org>
2017-12-02 00:31:47 +0530

    nightlight version update.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/clj-pune/pratham"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[nightlight/lein-nightlight "2.0.1"]]
+  :plugins [[nightlight/lein-nightlight "2.0.2"]]
   :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [org.clojure/clojurescript "1.9.946"]
                  [cheshire "5.8.0"]
-                 [nightlight "2.0.1"]])
+                 [nightlight "2.0.2"]])
